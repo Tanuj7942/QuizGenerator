612e82d6b8ca3dc3ad4f5ba64ba4bacfea33903d
Tanuj7942 <2337942.tarun.2001@gmail.com>
2022-09-11 00:42:51 +0530

    changes in login
    done with the animated tabs for the login system only left with the switching technique

--- GUI/JS/teachers-login.js
+++ GUI/JS/teachers-login.js
@@ -1,3 +1,27 @@
+function teacher_tab(){
+
+    const teach_login = document.querySelectorAll('.teacher-login-panel');
+    const stud_login = document.querySelectorAll('.student-login-panel');
+
+    for(const i of teach_login) {
+        i.classList.add('active');
+    }
+    for(const j of stud_login) {
+        j.classList.remove('active');
+    }
+}
+function student_tab(){
+
+    const teach_login = document.querySelectorAll('.teacher-login-panel');
+    const stud_login = document.querySelectorAll('.student-login-panel');
+
+    for(const i of stud_login) {
+        i.classList.add('active');
+    }
+    for(const j of teach_login) {
+        j.classList.remove('active');
+    }
+}
 function validation(){
     var userName = document.getElementById("username").value;
     var password = document.getElementById("password").value;
