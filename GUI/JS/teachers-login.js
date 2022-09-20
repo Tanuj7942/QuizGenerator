dcd60644f53f8d04f2498095cf03b055dd08691c
rosso <2337942.tarun.2001@gmail.com>
2022-09-20 15:29:10 +0530

    change in login
    completed the login for both teacher and student.Waiting for any changes and approval from client

--- GUI/JS/teachers-login.js
+++ GUI/JS/teachers-login.js
@@ -1,34 +1,55 @@
 function teacher_tab(){
 
-    const teach_login = document.querySelectorAll('.teacher-login-panel');
-    const stud_login = document.querySelectorAll('.student-login-panel');
+    document.getElementById('teacher-header').classList.add("active");
+    document.getElementById('student-header').classList.remove('active');
 
-    for(const i of teach_login) {
-        i.classList.add('active');
-    }
-    for(const j of stud_login) {
-        j.classList.remove('active');
-    }
+    document.getElementById('teacher-body').classList.add("active");
+    document.getElementById('student-body').classList.remove('active');
+
 }
 function student_tab(){
 
-    const teach_login = document.querySelectorAll('.teacher-login-panel');
-    const stud_login = document.querySelectorAll('.student-login-panel');
+    document.getElementById('student-header').classList.add("active");
+    document.getElementById('teacher-header').classList.remove('active');
 
-    for(const i of stud_login) {
-        i.classList.add('active');
-    }
-    for(const j of teach_login) {
-        j.classList.remove('active');
+    document.getElementById('student-body').classList.add("active");
+    document.getElementById('teacher-body').classList.remove('active');
+    
+}
+function visibility(visible){
+    if(visible.className == "material-icons teacher-visible-icon"){
+        toggle_visibility(visible, "teach_password");
+    } else {
+        toggle_visibility(visible, "stud_password");
     }
 }
-function validation(){
-    var userName = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if(userName == "" & password == ""){
-        alert("Blank Fields");
-        return false;
+function toggle_visibility(visible, element_id){
+    var login_pass = document.getElementById(element_id);
+    if(visible.textContent == "visibility_off"){ 
+        visible.textContent = "visibility";
+        login_pass.type = "text";
+
     } else {
+        visible.textContent = "visibility_off";
+        login_pass.type = "password";
+    }
+}
+function validation(check_class){
+    if(check_class.className == "login-essential-teacher") {
+        return check_validation("teach_username", "teach_password", "teach-login-tooltip");
+    } else {
+        return check_validation("stud_username", "stud_password", "stud-login-tooltip");
+    }
+}
+function check_validation(login_username, login_password, login_tooltip) {
+    var userName = document.getElementById(login_username).value.trim();
+    var password = document.getElementById(login_password).value.trim();
+    if(userName == "" || password == ""){
+        document.getElementById(login_tooltip).style.visibility = "visible";
+        return false;
+    }
+    else {
+        document.getElementById(login_tooltip).style.visibility = "hidden";
         return true;
     }
 }
