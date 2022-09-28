f65d580c1dcfc46575398107ec7b8a3d130387d7
rosso <2337942.tarun.2001@gmail.com>
2022-09-28 21:51:53 +0530

    changes in timer
    formatted the timer to double digit

--- GUI/JS/registration-confirmation.js
+++ GUI/JS/registration-confirmation.js
@@ -1,14 +1,21 @@
 const startingMinutes = 2;
-let time  = startingMinutes * 60;
+let time  = 10;
 
 const countdownEl = document.getElementById("timer");
-
-setInterval(updateCountdown, 1000);
+if(time == 0) {
+    resendOTP();
+} else {
+    setInterval(updateCountdown, 1000)
+}
 
 function updateCountdown () {
     const minutes = Math.floor(time/60);
     let seconds = time % 60;
 
-    countdownEl.innerHTML = `${0}${minutes}:${seconds}`;
+    countdownEl.innerHTML = `${(minutes).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}:${(seconds).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}`;
     time--;
+}
+
+function resendOTP () {
+    window.location("myntra.com");
 }
