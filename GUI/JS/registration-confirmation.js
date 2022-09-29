29d22817633b42615b7f65230183f36047353cb0
rosso <2337942.tarun.2001@gmail.com>
2022-09-29 23:58:49 +0530

    changes
    changed OTP size and remove some code from registration confirmation JS

--- GUI/JS/registration-confirmation.js
+++ GUI/JS/registration-confirmation.js
@@ -2,11 +2,6 @@ const startingMinutes = 2;
 let time  = 10;
 
 const countdownEl = document.getElementById("timer");
-if(time == 0) {
-    resendOTP();
-} else {
-    setInterval(updateCountdown, 1000);
-}
 
 function updateCountdown () {
     const minutes = Math.floor(time/60);
@@ -14,8 +9,4 @@ function updateCountdown () {
 
     countdownEl.innerHTML = `${(minutes).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}:${(seconds).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}`;
     time--;
-}
-
-function resendOTP () {
-    window.location("myntra.com");
 }
