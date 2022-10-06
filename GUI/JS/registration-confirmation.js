b90bd3271ed64a276a881d67cff2febeecfdde10
rosso <2337942.tarun.2001@gmail.com>
2022-10-06 16:00:18 +0530

    changes in registration confirmation
    created the OTP timer with redirecting feature to a servlet for sending an OTP through mail.

--- GUI/JS/registration-confirmation.js
+++ GUI/JS/registration-confirmation.js
@@ -1,9 +1,15 @@
-const startingMinutes = 2;
 let time  = 10;
 
 const countdownEl = document.getElementById("timer");
 
+setInterval(updateCountdown, 1000);
+
 function updateCountdown () {
+    if (time === 0) {
+        time = 10;
+        document.getElementById("new-otp-msg").style.visibility = "visible";
+        // window.location.href = "https://myntra.com";
+    }
     const minutes = Math.floor(time/60);
     let seconds = time % 60;
 
